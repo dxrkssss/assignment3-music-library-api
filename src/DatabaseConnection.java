1568891ce42b9c1566e19cb41d8cb85769f1dad6
alnur <alnure@list.ru>
2026-01-29 00:03:53 +0500

    feat(db): add DatabaseConnection and database schema

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,2 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String URL = "jdbc:sqlite:music.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
 }
