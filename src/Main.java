96e26db9164b7300ec8b7395ce0826eeacbe9070
alnur <alnure@list.ru>
2026-01-29 03:22:59 +0500

    feat(controller): add CLI controller and InputValidator

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
+import dto.MediaDTO;
+import controller.MediaController;
+
 public class Main {
+    public static void main(String[] args) {
+        MediaController controller = new MediaController();
+
+        MediaDTO song = new MediaDTO();
+        song.name = "Song One";
+        song.type = "SONG";
+        song.duration = 180;
+
+        MediaDTO podcast = new MediaDTO();
+        podcast.name = "Podcast One";
+        podcast.type = "PODCAST";
+        podcast.duration = 1200;
+
+        controller.create(song);
+        controller.create(podcast);
+
+        controller.showAll();
+    }
 }
---
+++ src/utils/InputValidator.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class InputValidator {
+    public static void checkName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new InvalidInputException("Name is empty");
+        }
+    }
+
+    public static void checkDuration(int duration) {
+        if (duration <= 0) {
+            throw new InvalidInputException("Duration must be positive");
+        }
+    }
+}
