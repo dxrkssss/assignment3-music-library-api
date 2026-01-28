ee6f1fa9f65948c05f9156436398a788f6c5958f
alnur <alnure@list.ru>
2026-01-29 03:54:27 +0500

    Minor changes in Main class

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
         MediaController controller = new MediaController();
 
         MediaDTO song = new MediaDTO();
-        song.name = "Song One";
+        song.name = "Save Your Tears";
         song.type = "SONG";
-        song.duration = 180;
+        song.duration = 216;
 
         MediaDTO podcast = new MediaDTO();
-        podcast.name = "Podcast One";
+        podcast.name = "Java Basics";
         podcast.type = "PODCAST";
-        podcast.duration = 1200;
+        podcast.duration = 1800;
 
         controller.create(song);
         controller.create(podcast);
